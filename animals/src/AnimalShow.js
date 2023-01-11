868059c5c733b1d11ccf3c4ec21932b98d5c4a39
Rafael Leites Luchese <rlluchese@gmail.com>
2023-01-11 11:59:42 -0300

    Rendering animals

--- animals/src/AnimalShow.js
+++ animals/src/AnimalShow.js
@@ -1,5 +1,5 @@
-function AnimalShow(){
-    return <div>Animal</div>;
+function AnimalShow({ type }){
+    return <div>{type}</div>;
 }
 
 export default AnimalShow;
--- animals/src/App.js
+++ animals/src/App.js
@@ -1,11 +1,30 @@
+import { useState } from 'react';
+import AnimalShow from './AnimalShow';
+
+function getRandomAnimal(){
+  const animals = ['bird','cat','cow','dog','gator','horse'];
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
-  const handleCLick = () => {
-    console.log('Click');
+  const [animals, setAnimals] = useState([]);
+
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
   };
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
 
   return (
     <div>
-      <button onClick={handleCLick}>Add Animal</button>
+      <button onClick={ handleClick }>
+        Add Animal
+      </button>
+      <div>
+        {renderedAnimals}
+      </div>
     </div>
   );
 }
